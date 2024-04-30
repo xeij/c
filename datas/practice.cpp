bb688347872fb772d79aab6321b52d6eef516429
xeij <q08d@aol.com>
2024-04-29 19:55:48 -0700

    4/29/2024

--- datas/practice.cpp
+++ datas/practice.cpp
@@ -5,23 +5,23 @@ using namespace std;
 int combinations(std::vector<int> items){
 
 int s=1;
-for(auto i : items){
-    if(items[i] != 0)
-    s *= items[i];
-    }
+    for(auto i : items){
+        if(items[i] != 0)
+        s *= items[i];
+        }
     return s;
 }
 
 bool profitableGamble(int prob, int prize, int pay){
 
-    if(prob * prize > pay){
-        return true;
-    }
-return false;
+        if(prob * prize > pay){
+            return true;
+        }
+    return false;
 }
 
 int main(){
 
-std::cout<< profitableGamble(10,200,50);
+    std::cout<< profitableGamble(10,200,50);
 
 }
