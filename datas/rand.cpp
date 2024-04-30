47ebd19f9b4441a31e3d2275de78a8d8b89788f7
xeij <q08d@aol.com>
2024-04-30 02:41:10 -0700

    4/30/2024

--- datas/rand.cpp
+++ datas/rand.cpp
@@ -25,8 +25,11 @@ bool tradeWorthy(int price, int peRatio, int twoHun_DailyMovingAverage){
     bool priceCondition = (price < twoHun_DailyMovingAverage );
     bool peRatioCondition = (peRatio < 20 );
     bool maCondition = (price < 0.95 * twoHun_DailyMovingAverage && price > 1.05 * twoHun_DailyMovingAverage );
+    bool betaCondition = (beta < 1);
+    bool dividendYieldCondition = (dividendYield > 2.0);
+    bool marketComparisonCondition = (price > marketIndexAverage);
 
-    return priceCondition && peRatioCondition && maCondition;
+    return priceCondition && peRatioCondition && maCondition && betaCondition && dividendYieldCondition && marketComparisonCondition;
 
 }
 
