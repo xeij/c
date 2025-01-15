3867414e0d17b18d6c68d2b81307cca1f9a71cc8
xeij <q08d@aol.com>
2025-01-14 21:10:25 -0800

    1/14/2025

---
+++ hackerrank/simpleArraySum.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    int i,n,sum;
+    cin>>n;
+    int a[n];
+    for(i=0; i<n; i++){
+        cin>>a[i];
+    }
+    sum = 0;
+    for(i=0; i<n; i++){
+        sum = sum + a[i];
+    }
+    cout<<sum;
+    return 0;
+}
